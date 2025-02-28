8a190392e95a87a52cd52a15979e090071f47545
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2025-02-28 10:44:36 +0530

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="Help center for Facilio">
+    <Layout title={`${siteConfig.title}`} description="Help center for Squad HR">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
